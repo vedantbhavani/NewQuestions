c48299ae52db860909b49055a9ba9a4f1d135f28
VS <98725762+vedantbhavani@users.noreply.github.com>
2024-04-26 11:07:25 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import person from './Answer'
+import { useRef } from 'react'
 
 function App() {
-  const showans = () => {
-    person.map((currElem) => {
-      console.log(currElem.answer);
-    })
+  const ref = useRef(null)
+  const showans = (i) => {
+    console.log(i.answer)
+    ref.current.classList.add("javabshow")
+    ref.current.innerHTML = `
+      <p className='px-5 javab'> ${i.answer}</p>
+    `
   }
   return (
-    <>
+    <>  
       <div className="constainer">
-        {person.map((i)=>{
+        {person.map((i) => {
           return <>
-          <div key={i} className="questions">
-          <p key={i} className='pt-3 ms-5 ques'>{i.question}
-            <span key={i} className='changeimg' onClick={showans}><img src="/src/down-arrow.png" alt="" /></span>
-          </p>
-          <p  className='px-5 javab'>{i.answer}</p>
-        </div>
-        </>
+            <div key={i} className="questions">
+              <p key={i} className='pt-3 ms-5 ques'>{i.question}
+                <span key={i} className='changeimg' onClick={() => showans(i)}><img src="/src/down-arrow.png" alt="" /></span>
+              </p>
+              <p ref={ref} className='px-5 javab'>{i.answer}</p>
+            </div>
+          </>
         })}
       </div>
     </>
